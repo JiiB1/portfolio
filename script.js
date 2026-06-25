8e6b72989b6f0d09e54c078687701457f348fd30
JiiB1 <jiibbottou@gmail.com>
2026-06-25 13:04:13 +0200

    refacto 2026

--- script.js
+++ script.js
@@ -1,20 +1,30 @@
-
 function onScrollingPage() {
-    var coef = window.scrollY / window.scrollMaxY;
-    var bar_bounding = this.document.getElementById("thebar").getBoundingClientRect();
-    var circle = this.document.getElementById("thecircle");
+  if (!window) {
+    return;
+  }
+  var coef = window.scrollY / window.scrollMaxY;
+  var bar_bounding = this.document
+    .getElementById("thebar")
+    .getBoundingClientRect();
+  var circle = this.document.getElementById("thecircle");
 
-    var value = bar_bounding.top + coef * bar_bounding.height -  0.5 * circle.getBoundingClientRect().height;
-    circle.style.top = value.toString()+"px";
+  var value =
+    bar_bounding.top +
+    coef * bar_bounding.height -
+    0.5 * circle.getBoundingClientRect().height;
+  circle.style.top = `${value.toString()}px`;
 }
 
 function resizeBars() {
-    var value = this.document.getElementById("main-header").getBoundingClientRect().height / 2;
-    this.document.getElementById("thebar").style.top = value.toString() + "px";
-    this.document.getElementById("thebar").style.bottom = this.document.getElementById("main-footer").getBoundingClientRect().height.toString() + "px";
-    onScrollingPage();
+  var value =
+    this.document.getElementById("main-header").getBoundingClientRect().height /
+    2;
+  this.document.getElementById("thebar").style.top = `${value.toString()}px`;
+  this.document.getElementById("thebar").style.bottom =
+    `${this.document.getElementById("main-footer").getBoundingClientRect().height.toString()}px`;
+  onScrollingPage();
 }
 
-window.addEventListener('scroll', onScrollingPage);
-window.addEventListener('resize', resizeBars);
+window?.addEventListener("scroll", onScrollingPage);
+window?.addEventListener("resize", resizeBars);
 resizeBars();
